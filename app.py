593982bc9911f5763334e2c33bd35970e31a644a
vindard <vinda.rd@gmail.com>
2018-10-03 22:49:32 +0700

    create(handler): setup handler function for client
    & add server function call

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ def algorithm(n: int) -> int:
 Address = Tuple[str, int]
 
 def handler(client: socket.socket) -> None:
-    pass
+    while True:
+        request: bytes = client.recv(100)
+        if not request.strip():
+            client.close()
+            return
+        number = int(request)
+        result = algorithm(number)
+        client.send(f'{result}\n'.encode('ascii'))   
 
 def server(address: Address) -> None:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,3 +25,5 @@ def server(address: Address) -> None:
         client, addr = sock.accept()
         print(f'Connection from{addr}')
         handler(client)
+
+server(('localhost', 30303))
