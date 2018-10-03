cfb55f7743994078fcc5c2f793a2dddd828d8937
vindard <vinda.rd@gmail.com>
2018-10-03 22:48:23 +0700

    create(server): setup server function

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
+from typing import *
+import socket
+
 def algorithm(n: int) -> int:
     return n + 42
+
+Address = Tuple[str, int]
+
+def handler(client: socket.socket) -> None:
+    pass
+
+def server(address: Address) -> None:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(5)
+    while True:
+        client, addr = sock.accept()
+        print(f'Connection from{addr}')
+        handler(client)
